295b58f9b687719d9d0435b474e7db5cd93b6026
Larry Jin <larry.jin@c3.ai>
2026-04-07 15:08:30 -0700

    fix frontend build error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: parseInt(process.env.PORT ?? "4173"),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: parseInt(env.PORT || "4173"),
+    },
+  };
 });
 
